eecf1b6210e1905e27a7b4117d0df99ae217443c
Nexustore <edsonbritonjunior@gmail.com>
2025-12-10 03:44:45 -0400

    Update search.routes.js

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -5,10 +5,10 @@ import { searchController } from "../controllers/search.controller.js";
 
 const router = Router();
 
-// POST /api/search/demo  → usado pelo front (fetch)
+// POST /api/search/demo  → usado pelo front
 router.post("/demo", searchController.demo);
 
-// GET /api/search/demo   → só pra você testar no navegador
+// GET /api/search/demo   → testar direto no navegador
 router.get("/demo", searchController.demo);
 
 export default router;
