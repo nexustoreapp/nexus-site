326b80e1d1265ab35a18a60703ef456bdc3a4487
Nexustore <edsonbritonjunior@gmail.com>
2025-12-11 06:22:40 -0400

    Update product.routes.js

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,11 +1,13 @@
 // backend/routes/product.routes.js
-
 import { Router } from "express";
 import { productController } from "../controllers/product.controller.js";
 
 const router = Router();
 
-// GET /api/product?id=1
-router.get("/", productController.real);
+// Lista todos (opcional, mas útil depois)
+router.get("/", productController.listAll);
+
+// Detalhe de um produto específico
+router.get("/:id", productController.getById);
 
 export default router;
