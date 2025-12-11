a8bf7d0a3f4a5a231578e9b4342e1d95e19a93d2
Nexustore <edsonbritonjunior@gmail.com>
2025-12-11 03:48:03 -0400

    Update search.routes.js

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,12 +1,10 @@
-// backend/routes/search.routes.js
-
 import { Router } from "express";
 import { searchController } from "../controllers/search.controller.js";
 
 const router = Router();
 
-// ROTA REAL: /api/search  (GET e POST)
-router.post("/", searchController.real);
+// /api/search
 router.get("/", searchController.real);
+router.post("/", searchController.real);
 
 export default router;
