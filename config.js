5f93cd1b63f09dc9140cf2156b37fbe72c389c9e
Nexustore <edsonbritonjunior@gmail.com>
2025-12-13 13:26:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 window.NEXUS_API_BASE =
-  location.hostname === "localhost"
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://SEU_BACKEND_NO_RENDER.onrender.com";
+    : "http://localhost:3000";
