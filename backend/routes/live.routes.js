77c8c2979f171875d79ef5e0f6ce2a57d1784a37
Nexustore <edsonbritonjunior@gmail.com>
2025-12-30 12:13:46 -0400

    Atualizar o live.routes.js

--- backend/routes/live.routes.js
+++ backend/routes/live.routes.js
@@ -3,6 +3,7 @@ import { liveCatalogController } from "../controllers/liveCatalog.controller.js"
 
 const router = Router();
 
+router.post("/request-batch", liveCatalogController.requestBatch);
 router.post("/request", liveCatalogController.request);
 router.get("/jobs", liveCatalogController.jobs);
 router.post("/update", liveCatalogController.update);
