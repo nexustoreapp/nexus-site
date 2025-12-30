9199512cea798fa6058fd4ec2181c6ef7001b334
Nexustore <edsonbritonjunior@gmail.com>
2025-12-30 11:16:36 -0400

    Criar live.routes.js

---
+++ backend/routes/live.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { liveCatalogController } from "../controllers/liveCatalog.controller.js";
+
+const router = Router();
+
+router.post("/request", liveCatalogController.request);
+router.get("/jobs", liveCatalogController.jobs);
+router.post("/update", liveCatalogController.update);
+router.get("/get", liveCatalogController.get);
+
+export default router;
