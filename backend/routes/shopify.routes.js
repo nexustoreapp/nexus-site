177a0fb9f4760d89119e5534d9a33ba4327b0ab5
Nexustore <edsonbritonjunior@gmail.com>
2025-12-31 12:44:23 -0400

    Atualizar o shopify.routes.js

--- backend/routes/shopify.routes.js
+++ backend/routes/shopify.routes.js
@@ -4,5 +4,6 @@ import { shopifyController } from "../controllers/shopify.controller.js";
 const router = Router();
 
 router.get("/products", shopifyController.products);
+router.get("/product", shopifyController.productByHandle);
 
 export default router;
