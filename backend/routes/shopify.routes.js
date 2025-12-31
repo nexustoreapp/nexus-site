3b851ea1a4f5001a619de28ddd234c15085fed69
Nexustore <edsonbritonjunior@gmail.com>
2025-12-31 15:53:29 -0400

    Delete backend/routes/shopify.routes.js

--- backend/routes/shopify.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { shopifyController } from "../controllers/shopify.controller.js";
-
-const router = Router();
-
-router.get("/products", shopifyController.products);
-router.get("/product", shopifyController.productByHandle);
-
-export default router;
