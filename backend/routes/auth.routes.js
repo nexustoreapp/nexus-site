40ac4ae86736afae263be9ebe1cdee0ae8085732
Nexustore <edsonbritonjunior@gmail.com>
2026-01-04 12:31:33 -0400

    Atualizar o auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,11 @@
+// backend/routes/auth.routes.js
 import express from "express";
-import {
-  register,
-  login,
-  verifyOtp
-} from "../controllers/auth.controller.js";
+import { register, login, verifyOtp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
 router.post("/verify-otp", verifyOtp);
+router.post("/login", login);
 
 export default router;
