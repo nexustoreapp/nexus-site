a22a7b8233bd0b7685c44d8f6699244d3c252cef
Nexustore <edsonbritonjunior@gmail.com>
2026-01-14 02:59:16 -0400

    Atualizar o search.routes.js

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,9 +1,9 @@
-// backend/routes/search.routes.js
 import { Router } from "express";
 import { search } from "../controllers/search.controller.js";
+import { antiScraping } from "../middlewares/antiScraping.middleware.js";
 
 const router = Router();
 
-router.get("/", search);
+router.get("/", antiScraping, search);
 
 export default router;
