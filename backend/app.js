0c529f841a93ce2a2fd117449782656862006c22
Nexustore <edsonbritonjunior@gmail.com>
2026-01-14 18:01:13 -0400

    Atualizar o app.js

--- backend/app.js
+++ backend/app.js
@@ -5,29 +5,20 @@ import dotenv from "dotenv";
 
 import apiRoutes from "./routes/index.js";
 import { rateLimiter } from "./middlewares/rateLimit.middleware.js";
-import { securityHeaders } from "./middlewares/security.middleware.js";
+import { securityLogger } from "./middlewares/securityLog.middleware.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 dotenv.config();
 
 const app = express();
 
-/* ===============================
-   MIDDLEWARES BASE
-================================ */
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
-app.use(securityHeaders);
+app.use(securityLogger);
 
-/* ===============================
-   ROTAS
-================================ */
 app.use("/api", apiRoutes);
 
-/* ===============================
-   404 PADRÃO
-================================ */
 app.use((req, res) => {
   res.status(404).json({
     ok: false,
@@ -36,9 +27,6 @@ app.use((req, res) => {
   });
 });
 
-/* ===============================
-   ERROR HANDLER
-================================ */
 app.use(errorHandler);
 
 export default app;
