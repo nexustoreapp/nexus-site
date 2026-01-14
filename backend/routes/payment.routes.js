7a625d1d12121aa2ef03897eff73fdd623e05815
Nexustore <edsonbritonjunior@gmail.com>
2026-01-14 01:31:22 -0400

    Atualizar o payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,15 +1,11 @@
-// backend/routes/payment.routes.js
 import { Router } from "express";
 import { createPayment } from "../controllers/payment.controller.js";
 import { paymentWebhook } from "../controllers/payment.webhook.js";
-import { requireAuth } from "../middlewares/auth.middleware.js"; // ✅ PLURAL
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-// cria pagamento (usuário logado)
 router.post("/create", requireAuth, createPayment);
-
-// webhook do gateway (não usa auth)
 router.post("/webhook", paymentWebhook);
 
 export default router;
