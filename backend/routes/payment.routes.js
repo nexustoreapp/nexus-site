e88cb0c1da50857fd1e1d5ebfb40d91142e7e8c3
Nexustore <edsonbritonjunior@gmail.com>
2026-01-14 02:14:13 -0400

    Atualizar o payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,22 +1,12 @@
-// backend/routes/payment.routes.js
 import { Router } from "express";
 import { createPayment } from "../controllers/payment.controller.js";
 import { paymentWebhook } from "../controllers/payment.webhook.js";
 import { requireAuth } from "../middlewares/auth.middleware.js";
-import { antiReplay } from "../middlewares/antiReplay.middleware.js";
+import { paymentLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const router = Router();
 
-/*
-  Pagamento protegido:
-  - login obrigatório
-  - nonce anti replay
-*/
-router.post("/create", requireAuth, antiReplay, createPayment);
-
-/*
-  Webhook NÃO usa auth nem antifraude
-*/
+router.post("/create", requireAuth, paymentLimiter, createPayment);
 router.post("/webhook", paymentWebhook);
 
 export default router;
