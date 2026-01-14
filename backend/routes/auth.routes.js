59d9ddf4bc0e329ebd564da614ea1560247bbdc2
Nexustore <edsonbritonjunior@gmail.com>
2026-01-14 02:36:52 -0400

    Atualizar o auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,14 @@
+// backend/routes/auth.routes.js
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller.js";
 import { authLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const router = Router();
 
+// Registro
 router.post("/register", authLimiter, register);
+
+// Login
 router.post("/login", authLimiter, login);
 
 export default router;
