e9839f4f8ac9fd13b3c4c6b95dfe00d4eb89d0b2
Edson <edsonbritonjunior@gmail.com>
2026-02-06 07:07:08 -0400

    Excluir orders.routes.js

--- backend/orders/orders.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrder } from "./orders.controller.js";
-import { requireAuth } from "../middlewares/auth.middleware.js";
-
-const router = Router();
-
-router.post("/create", requireAuth, createOrder);
-
-export default router;
