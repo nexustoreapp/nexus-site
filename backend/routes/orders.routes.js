ba0d83641ba57d3ad0ca874e7d5a8a6954e4b909
Edson <edsonbritonjunior@gmail.com>
2026-02-07 19:12:11 -0400

    Atualizar o orders.routes.js

--- backend/routes/orders.routes.js
+++ backend/routes/orders.routes.js
@@ -5,8 +5,14 @@ import ordersStore from "../data/orders.store.js";
 const router = Router();
 
 router.get("/", requireAuth, (req, res) => {
-  const userOrders = ordersStore.filter(o => o.userId === req.user.id);
-  res.json({ ok: true, orders: userOrders });
+  const userOrders = ordersStore.filter(
+    o => o.userId === req.user.id
+  );
+
+  res.json({
+    ok: true,
+    orders: userOrders
+  });
 });
 
 export default router;
