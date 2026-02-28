64d69b241e76bedbccb80b253f48f0e6ec845745
Edson <edsonbritonjunior@gmail.com>
2026-02-28 09:26:09 -0400

    Atualizar o payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createPayment } from "../controllers/payment.controller.js";
+import { createPaymentController } from "../controllers/payment.controller.js";
 import { paymentWebhook } from "../controllers/payment.webhook.js";
-import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/create", requireAuth, createPayment);
+router.post("/create", createPaymentController);
 router.post("/webhook", paymentWebhook);
 
 export default router;
