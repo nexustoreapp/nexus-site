e675466f2ee9232d44b814c5d45aa0faaea8905e
Edson <edsonbritonjunior@gmail.com>
2026-03-02 19:01:14 -0400

    Atualizar o payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,14 +1,10 @@
-// backend/routes/payment.routes.js
 import { Router } from "express";
 import { createPaymentController } from "../controllers/payment.controller.js";
 import { mercadopagoWebhook } from "../controllers/payment.webhook.js";
 
 const router = Router();
 
-// /api/v1/payment
 router.post("/create", createPaymentController);
-
-// webhook público (não exige auth)
-router.post("/webhook/mercadopago", mercadopagoWebhook);
+router.post("/webhook", mercadopagoWebhook);
 
 export default router;
