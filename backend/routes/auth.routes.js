2f02ef4d658f0b633940ef0d3bd8b372cc6346c3
Edson <edsonbritonjunior@gmail.com>
2026-03-04 16:40:35 -0400

    Atualizar o auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,18 +1,35 @@
 // backend/routes/auth.routes.js
 import { Router } from "express";
+
 import {
   registerController,
   loginController,
   meController
 } from "../controllers/auth.controller.js";
+
 import { requireAuth } from "../middlewares/auth.middleware.js";
+import { deleteAccount } from "../controllers/deleteAccount.controller.js";
 
 const router = Router();
 
+/* =====================================
+REGISTER
+===================================== */
 router.post("/register", registerController);
+
+/* =====================================
+LOGIN
+===================================== */
 router.post("/login", loginController);
 
-// ✅ rota que faltava pro front confirmar sessão
+/* =====================================
+CONFIRMAR SESSÃO
+===================================== */
 router.get("/me", requireAuth, meController);
+
+/* =====================================
+DELETAR CONTA
+===================================== */
+router.delete("/delete", requireAuth, deleteAccount);
 
 export default router;
