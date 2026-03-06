c8e191dd30fd89d2a85cf9187bb292b064a7fce1
Edson <edsonbritonjunior@gmail.com>
2026-03-06 12:02:46 -0400

    Atualizar o server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
    ROTAS API
 ========================= */
 
-app.use("/api/products", productRoutes);
+app.use("/api", productRoutes);
 
 app.use("/api/v1/payment", paymentRoutes);
 
