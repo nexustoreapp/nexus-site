56c0e0f204a84130e92ce575a9498e8a898a72a8
Edson <edsonbritonjunior@gmail.com>
2026-03-05 20:48:31 -0400

    Atualizar o checkout.routes.js

--- backend/routes/checkout.routes.js
+++ backend/routes/checkout.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { prepareCheckout } from "../controllers/checkout.controller.js";
-import { requireAuth } from "../middlewares/auth.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/prepare", requireAuth, prepareCheckout);
+router.post("/prepare", authMiddleware, prepareCheckout);
 
 export default router;
